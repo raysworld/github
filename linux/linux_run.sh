1eb0f32d6d3822fe237f29c4cfdcec1a129a2b88
Rui LI <raysworld@outlook.com>
2020-06-22 15:38:12 +0800

    change to relative path

--- linux/linux_run.sh
+++ linux/linux_run.sh
@@ -2,7 +2,7 @@
 
 BASE_PATH=$(dirname $(readlink -f $0))
 
-python ${BASE_PATH}/github_hosts.py
+python ${BASE_PATH}/../github_hosts.py
 if [ $? -ne 0 ]; then
     echo "get host name error"
     exit 1
@@ -12,14 +12,14 @@ sed -i "/GitHub Start/d" /etc/hosts
 sed -i "/GitHub End/d" /etc/hosts
 
 # Clear /etc/hosts first
-for i in `cat ${BASE_PATH}/github_domain.txt`; do
+for i in `cat ${BASE_PATH}/../github_domain.txt`; do
     sed -i "/$i/d" /etc/hosts
 done
 
 # Add dns record at /etc/hosts
 IFS_OLD=$IFS
 IFS=$'\n'
-ALINES=$(cat ${BASE_PATH}/github_hosts.txt)
+ALINES=$(cat ${BASE_PATH}/../github_hosts.txt)
 for ALINE in $ALINES;do
     echo $ALINE >> /etc/hosts
 done
